cd410093058a2190fb0b86604fb360a55da76a84
tobi <awogbemilatobi@gmail.com>
2023-12-09 09:52:14 +0100

    commit

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,65 +1,87 @@
 import { useState } from "react";
 import "./styles.css";
 
+const tipOptions = {
+  "Disatisfied (0%)": 0,
+  "it was ok (5%)": 0.05,
+  "it was good (10%)": 0.1,
+  "Absolutely amazing (20%)": 0.2,
+};
+
+const options = Object.keys(tipOptions);
+
 export default function App() {
+  const [billAmount, setBillAmount] = useState(0);
+  const [serviceRating, setServiceRating] = useState("");
+  const [friendRating, setFriendRating] = useState("");
+
+  const handleBillChange = (event) => {
+    setBillAmount(event.target.value);
+  };
+
+  const handleServiceChange = (event) => {
+    setServiceRating(event.target.value);
+  };
+
+  const handleFriendChange = (event) => {
+    setFriendRating(event.target.value);
+  };
+
+  const calculateTip = (rating, bill) => {
+    return bill * tipOptions[rating];
+  };
+
   return (
-    <div className="App">
-      <Counter />
+    <div>
+      <form>
+        <label>How much was the bill?</label>
+        <input type="number" value={billAmount} onChange={handleBillChange} />
+      </form>
+
+      <div>
+        <label>How did you like the service?</label>
+        <select value={serviceRating} onChange={handleServiceChange}>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <form>
+        <label>How did your friend like the service?</label>
+        <select value={friendRating} onChange={handleFriendChange}>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </form>
+
+      <DisplayInput
+        bill={Number(billAmount)}
+        service={serviceRating}
+        friend={friendRating}
+        calculateTip={calculateTip} // Pass the calculateTip function to DisplayInput
+      />
     </div>
   );
 }
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+function DisplayInput({ bill, service, friend, calculateTip }) {
+  const tipForYou = calculateTip(service, bill);
+  const tipForFriend = calculateTip(friend, bill);
 
-  function handleReset() {
-    setCount(0);
-    setStep(1);
-  }
-
-  const date = new Date("june 21 2027");
-  date.setDate(date.getDate() + count);
+  const totalBill = Math.round(bill + tipForYou + tipForFriend);
+  const totalTip = Math.round(tipForFriend + tipForYou);
 
   return (
     <div>
-      <div>
-        <input
-          type="range"
-          min="0"
-          max="10"
-          value={step}
-          onChange={(e) => setStep(Number(e.target.value))}
-        />
-        <span>Step: {step}</span>
-      </div>
-
-      <div>
-        <button onClick={() => setCount((c) => c - step)}>-</button>
-        <input
-          type="text"
-          value={count}
-          onChange={(e) => setCount(Number(e.target.value))}
-        />
-        <button onClick={() => setCount((c) => c + step)}>+</button>
-      </div>
-
-      <p>
-        <span>
-          {count === 0
-            ? "Today is "
-            : count > 0
-            ? `${count} days from today is `
-            : `${Math.abs(count)} days ago was `}
-        </span>
-        <span>{date.toDateString()}</span>
-      </p>
-
-      {count !== 0 || step !== 1 ? (
-        <div>
-          <button onClick={handleReset}>Reset</button>
-        </div>
-      ) : null}
+      <h3>
+        You pay ${totalBill} (${bill} + ${totalTip} tip)
+      </h3>
     </div>
   );
 }
